15086ae6d142703a66a6445f5b80f7f995494821
Troy Connor <troy0820@gmail.com>
2019-03-04 19:32:35 -0500

    🗃[index]Add eslint and config to repository

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+'use strict';
 //command line app 3 digit number try to guess
 //perfect good bad
 //-d number of digits prompt user for answer
@@ -12,8 +13,7 @@ program
 
 
 function generateNumber(digits) {
-    //	 Math.floor(Math.random() * Math.floor(max));
-    let arr = [];
+    const arr = [];
     for (let i = 0; i < digits; i++) {
         arr.push(Math.ceil(Math.random() * Math.floor(9)));
     }
@@ -24,51 +24,50 @@ async function main() {
     if (program.digits) {
 
         const newAnswer = generateNumber(program.digits);
-        //console.log('numbers', newAnswer);
-        let loop = true;
+        const loop = true;
         while (loop) {
-            let  guess = await promptly.prompt('Enter your guess');
-            let  answers = checkAnswer(guess,newAnswer);
-	   console.log("answers", answers);
-	    let perfect = 0;
-	  for (let i=0; i < newAnswer.length; i++) {
-	  	if ("perfect" == answers[i]) {
-			perfect++;
-		}
-	  }
-		if (perfect == answers.length) {
-			console.log("You got it")
-			break;
-		}
+            const guess = await promptly.prompt('Enter your guess');
+            const answers = checkAnswer(guess, newAnswer);
+            console.log('answers', answers);
+            let perfect = 0;
+            for (let i = 0; i < newAnswer.length; i++) {
+                if ('perfect' == answers[i]) {
+                    perfect++;
+                }
+            }
+            if (perfect == answers.length) {
+                console.log('You got it');
+                break;
+            }
 
 
         }
     } else {
-        console.log('Check input to program: needs -d with number')
+        console.log('Check input to program: needs -d with number');
     }
 }
 
 function checkAnswer(guess, answer) {
-	let arr = [];
-	for (let i=0; i < guess.length; i++) {
-		if (guess[i] == answer[i]) {
-			arr.push("perfect");
-		} else if (checkIncludes(answer, guess[i])) {
-			arr.push("good");
-		} else {
-			arr.push("bad");
-		}
-	}
-	return arr;
+    const arr = [];
+    for (let i = 0; i < guess.length; i++) {
+        if (guess[i] == answer[i]) {
+            arr.push('perfect');
+        } else if (checkIncludes(answer, guess[i])) {
+            arr.push('good');
+        } else {
+            arr.push('bad');
+        }
+    }
+    return arr;
 }
 
 function checkIncludes(answer, guess) {
-	for (let i=0; i < answer.length; i++) {
-		if (answer[i] == guess) {
-			return true;
-		}
-	}
-	return false;
+    for (let i = 0; i < answer.length; i++) {
+        if (answer[i] == guess) {
+            return true;
+        }
+    }
+    return false;
 }
 
 main();
