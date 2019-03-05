a71db292ddef85afacf5b82babbfb7ace1b666a3
Troy Connor <troy0820@gmail.com>
2019-03-04 19:47:56 -0500

    🗃[test]Update variable in test to pass linting

--- guess_test.go
+++ guess_test.go
@@ -16,11 +16,11 @@ func TestCheckIncludes(t *testing.T) {
 
 func TestCheckAnswer(t *testing.T) {
 	guess := []int{3, 4, 5}
-	test_guess := []int{3, 4, 5}
+	testGuess := []int{3, 4, 5}
 	answer := []string{"perfect", "perfect", "perfect"}
 
-	if want, got := true, reflect.DeepEqual(answer, checkAnswer(guess, test_guess)); got != want {
-		t.Errorf("Error: This is what we want %+v, and this is what you got %+v", answer, checkAnswer(guess, test_guess))
+	if want, got := true, reflect.DeepEqual(answer, checkAnswer(guess, testGuess)); got != want {
+		t.Errorf("Error: This is what we want %+v, and this is what you got %+v", answer, checkAnswer(guess, testGuess))
 	}
 
 }
