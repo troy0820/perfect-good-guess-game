437df43c9cfcb161f1ac5f60a5451ecc7c131fa2
Troy Connor <troy0820@gmail.com>
2019-03-12 09:33:25 -0400

    🗃[test]Add test to check false in guess_test.go

--- guess_test.go
+++ guess_test.go
@@ -11,7 +11,13 @@ func TestCheckIncludes(t *testing.T) {
 
 	t.Run("Test if checkIncludes returns true", func(t *testing.T) {
 		if want, got := true, checkIncludes(guess, answer); got != want {
-			t.Errorf("Error: This is what you we want %+v, and this is what you got %+v", want, got)
+			t.Errorf("Error: This is what we want %+v, and this is what you got %+v", want, got)
+		}
+	})
+	wrong := 5
+	t.Run("Test if checkincludes returns false", func(t *testing.T) {
+		if want, got := false, checkIncludes(guess, wrong); got != want {
+			t.Errorf("Error: This is what we want %+v, and this is what we got %+v", want, got)
 		}
 	})
 }
@@ -24,6 +30,12 @@ func TestCheckAnswer(t *testing.T) {
 	t.Run("Test if answer is true", func(t *testing.T) {
 		if want, got := true, reflect.DeepEqual(answer, checkAnswer(guess, testGuess)); got != want {
 			t.Errorf("Error: This is what we want %+v, and this is what you got %+v", answer, checkAnswer(guess, testGuess))
+		}
+	})
+	wrong := []int{6, 7, 8}
+	t.Run("Test if answer is false", func(t *testing.T) {
+		if want, got := false, reflect.DeepEqual(answer, checkAnswer(guess, wrong)); got != want {
+			t.Errorf("Error: This is what we want %+v, and this is what you got %+v", answer, checkAnswer(guess, wrong))
 		}
 	})
 }
